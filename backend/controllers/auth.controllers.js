d1e2f853bd1b57c88df1ede622daa854c70a4af6
komal <komalpanda1510@gmail.com>
2026-03-11 23:38:40 +0530

    Backend setup with user model, JWT authentication, and database connection

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -1,10 +1,10 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
 import genToken from "../utils/genToken.js";
-const signUp = async (req, res) => {
+export const signUp = async (req, res) => {
   try {
     const { fullName, email, password, mobile, role } = req.body;
-    const user = await User.findOne({ email });
+    let user = await User.findOne({ email });
     if (user) {
       return res.status(400).json({ message: "User already exists" });
     }
@@ -28,5 +28,49 @@ const signUp = async (req, res) => {
       password: hashedPassword,
     });
     const token = await genToken(user._id);
-  } catch (error) {}
+    res.cookie("token", token, {
+        secure:false,
+        httpOnly:true,
+        sameSite:"strict",
+        maxAge:7*24*60*60*1000
+    })
+    res.status(201).json(user);
+  } catch (error) {
+    return res.status(500).json(`signup error: ${error}`);
+  }
+};
+
+export const signIn = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    let user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "User does not exists" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+    const token = await genToken(user._id);
+    res.cookie("token", token, {
+        secure:false,
+        httpOnly:true,
+        sameSite:"strict",
+        maxAge:7*24*60*60*1000
+    })
+    res.status(200).json(user);
+  } catch (error) {
+    return res.status(500).json(`signIn error: ${error}`);
+  }
+};
+
+
+export const signOut = async (req, res) => {
+  try {
+    res.clearCookie("token");
+    return res.status(200).json({ message: "Signout successful" });
+  } catch (error) {
+    return res.status(500).json({ message: "Logout error" });
+  }
 };
