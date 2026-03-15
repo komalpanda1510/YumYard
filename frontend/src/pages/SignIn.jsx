8398d58fb50b9fb00944774243f9017e883d33eb
komal <komalpanda1510@gmail.com>
2026-03-15 22:31:26 +0530

    feat: implement signin page with authentication API integration

--- frontend/src/pages/SignIn.jsx
+++ frontend/src/pages/SignIn.jsx
@@ -1,11 +1,128 @@
-import React from 'react'
+import React, { useState } from "react";
+import { FaRegEye } from "react-icons/fa";
+import { FaRegEyeSlash } from "react-icons/fa";
+import { FcGoogle } from "react-icons/fc";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { serverUrl } from "../App";
 
 function SignIn() {
+  const primaryColor = "#ff4d2d";
+  const hoverColor = "#e64323";
+  const bgColor = "#fff9f6";
+  const borderColor = "#ddd";
+  const [showPassword, setShowPassword] = useState(false);
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+
+  const handleSignIn = async () => {
+    try {
+      const result = await axios.post(`${serverUrl}/api/auth/signin`,{
+        email, password
+      },{withCredentials: true})
+      console.log(result)
+    } catch (error) {
+      console.log(error)
+    }
+  }
   return (
-    <div>
-      sign in page
+    <div
+      className="min-h-screen w-full flex items-center justify-center p-4 
+    "
+      style={{ backgroundColor: bgColor }}
+    >
+      <div
+        className={`bg-white rounded-xl shadow-lg w-full max-w-md p-8 border `}
+        style={{
+          border: `1px solid ${borderColor}`,
+        }}
+      >
+        <h1
+          className={`text-3xl font-bold mb-2 `}
+          style={{ color: primaryColor }}
+        >
+          YumYard
+        </h1>
+        <p className="text-grey-600 mb-8">
+          Sign In to your account to get started with delicious food deliveries .
+        </p>
+
+        
+
+        {/*email*/}
+        <div>
+          <label
+            htmlFor="email"
+            className="block text-grey-700 font-medium mb-1"
+          >
+            Email
+          </label>
+          <input
+            type="email"
+            className="w-full border rounded-lg px-3 py-2 focus:outline-none 
+          "
+            placeholder="Enter your Email"
+            style={{
+              border: `1px solid ${borderColor}`,
+            }}
+            onChange={(e)=>setEmail(e.target.value)} value={email}
+          />
+        </div>
+
+       
+
+        {/*password*/}
+        <div>
+          <label
+            htmlFor="password"
+            className="block text-grey-700 font-medium mb-1"
+          >
+            Password
+          </label>
+          <div className="relative">
+            <input
+              type={`${showPassword ? "text" : "password"}`}
+              className="w-full border rounded-lg px-3 py-2 focus:outline-none 
+          "
+              placeholder="Enter your Password"
+              style={{
+                border: `1px solid ${borderColor}`,
+              }}
+              onChange={(e)=>setPassword(e.target.value)} value={password}
+            />
+            <button
+              className="absolute cursor-pointer right-3 top-3.5 text-gray-500"
+              onClick={() => setShowPassword((prev) => !prev)}
+            >
+              {!showPassword ? <FaRegEye /> : <FaRegEyeSlash />}
+            </button>
+          </div>
+        </div>
+
+        <div className="text-right mb-4 text-[#ff4d2d] cursor-pointer font-medium" onClick={()=>navigate("/forgot-password")}>
+          Forgot Password  
+        </div>
+        
+        <button
+          className={`w-full font-semibold py-2 mt-4 rounded-lg transition duration-200 bg-[#ff4d2d] text-white hover:bg-[#e64323] cursor-pointer`}
+          onClick={handleSignIn}
+        >
+          Sign In
+        </button>
+
+        <button className="w-full mt-4 flex items-center justify-center gap-2 border rounded-lg py-2 px-4 transition duration-200 border-gray-400 hover:bg-gray-100 cursor-pointer">
+          <FcGoogle size={20} />
+          <span>Sign In with google</span>
+        </button>
+        <p className="text-center mt-6 cursor-pointer" onClick={()=>navigate("/signup")}>
+          Want to create a new account ?{" "}
+          <span className="text-[#ff4d2d]">Sign Up</span>
+        </p>
+      </div>
     </div>
-  )
+  );
 }
 
-export default SignIn
+export default SignIn;
