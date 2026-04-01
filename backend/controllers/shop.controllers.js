5bf228b932a0dea0cc1aa443026dced09a96abd9
komal <komalpanda1510@gmail.com>
2026-04-01 20:46:38 +0530

    feat(item): add and update item with image upload and shop ownership validation

---
+++ backend/controllers/shop.controllers.js
@@ -0,0 +1,38 @@
+import uploadOnCloudinary from "../utils/cloudinary.js";
+import Shop from "../models/shop.model.js";
+export const createEditShop = async (req, res) => {
+  try {
+    const { name, city, state, address } = req.body;
+    let image;
+    if (req.file) {
+      image = await uploadOnCloudinary(req.file.path);
+    }
+    let shop = await Shop.findOne({ owner: req.userId });
+    if (!shop) {
+      shop = await Shop.create({
+        name,
+        city,
+        state,
+        address,
+        image,
+        owner: req.userId,
+      });
+    }else{
+        shop = await Shop.findByIdAndUpdate(shop._id,{
+        name,
+        city,
+        state,
+        address,
+        image,
+        owner: req.userId,
+      },      {new:true});
+    }
+
+    await shop.populate("owner");
+    res.status(201).json(shop);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Failed to create shop", error: error.message });
+  }
+};
