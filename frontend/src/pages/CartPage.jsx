42d2f49edd0e1f0b014076428fa42999b360707e
komal <komalpanda1510@gmail.com>
2026-04-27 05:07:40 +0530

    feat: display cart items with total amount and add checkout button in CartPage

--- frontend/src/pages/CartPage.jsx
+++ frontend/src/pages/CartPage.jsx
@@ -6,7 +6,7 @@ import CartItemCard from "../components/CartItemCard";
 
 function CartPage() {
     const navigate = useNavigate();
-    const {cartItems} = useSelector(state=> state.user);
+    const {cartItems, totalAmount} = useSelector(state=> state.user);
   return (
     <div className="min-h-screen bg-[#fff9f6] flex justify-center p-6">
       <div className="w-full max-w-[800px]">
@@ -21,12 +21,24 @@ function CartPage() {
         </div>
         {cartItems?.length == 0 ? (
             <p className="text-gray-500 text-lg text-center">Your cart is empty.</p>
-        ) : (
+        ) : (<>
          <div className="space-y-4">
           {cartItems?.map((item, index) => (
             <CartItemCard key={index} data={item} />
           ))}
          </div>
+         <div className="mt-6 bg-white p-4 rounded-xl shadow flex justify-between
+         items-center border">
+             <h1 className="text-lg font-semibold">Total Amount</h1>
+             <span className="text-xl font-bold text-[#ff4d2d]">₹{totalAmount}</span>
+         </div>
+         <div className="mt-4 flex justify-end">
+          <button className="bg-[#ff4d2d] text-white px-6 py-3 rounded-lg text-lg
+          font-medium hover:bg-[#e64526] transition cursor-pointer">
+            Proceed To CheckOut
+          </button>
+         </div>
+         </>
         )}
       </div>
     </div>
