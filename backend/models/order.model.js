260d9acacb21f465b1c5db7e5184a061def06bc2
komal <komalpanda1510@gmail.com>
2026-04-28 22:28:46 +0530

    feat: add order and shop order schema for multi-shop checkout

--- backend/models/order.model.js
+++ backend/models/order.model.js
@@ -51,3 +51,6 @@ const orderSchema = new mongoose.Schema(
   },
   { timestamps: true },
 );
+
+const Order = mongoose.model("Order", orderSchema)
+export default Order
--- frontend/src/pages/CheckOut.jsx
+++ frontend/src/pages/CheckOut.jsx
@@ -1,11 +1,37 @@
-import React from 'react'
+import React from "react";
+import { IoIosArrowRoundBack } from "react-icons/io";
+import { FaLocationDot } from "react-icons/fa6";
+import { IoMdSearch } from "react-icons/io";
+import { BiCurrentLocation } from "react-icons/bi";
 
 function CheckOut() {
   return (
-    <div>
-      
+    <div className="min-h-screen bg-[#fff96f6] flex items-center justify-center p-6">
+      <div
+        className="absolute top-[20px] left-[20px]  z-[10]"
+        onClick={() => navigate("/")}
+      >
+        <IoIosArrowRoundBack size={35} className="text-[#ff4d2d]" />
+      </div>
+      <div className="w-full max-w-[900px] bg-white rounded-2xl shadow-xl p-6 space-y-6">
+        <h1 className="text-2xl font-bold text-gray-800">Checkout</h1>
+        <section>
+          <h2 className="text-lg font-semibold mb-2 flex items-center gap-2 text-gray-800">
+            <FaLocationDot className="text-[#ff4d2d]" />
+            Delivery Location
+          </h2>
+          <div className="flex gap-2 mb-3">
+            <input type="text" className="flex-1 border border-gray-300 rounded-lg
+             p-2 text-sm focus:outline-none focus:ring-2 focus:ring-[#ff4d2d]" placeholder="Enter your Delivery Address..."/>
+            <button className="bg-[#ff4d2d] hover:bg-[#e64526] text-white
+            px-3 py-2 rounded-lg flex items-center justify-center "><IoMdSearch size={17}/></button>
+            <button className="bg-blue-500 hover:bg-blue-600 text-white 
+            px-3 py-2 rounded-lg flex items-center justify-center"><BiCurrentLocation size={17}/></button>
+          </div>
+        </section>
+      </div>
     </div>
-  )
+  );
 }
 
-export default CheckOut
+export default CheckOut;
